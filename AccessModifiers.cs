5c91d65474841267fd3a027104fe1690b098b52b
Jordan Castelloe <jordancastelloe@Jordans-MacBook-Pro.local>
2019-04-24 07:13:45 -0400

    starter example code for classes demo

---
+++ AccessModifiers.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace classes {
+    class tree{
+    
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace classes
+{
+    // Create a class to represent a sandwich 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a few new instances of sandwiches
+
+            // Create a new list called menu and add some sandwiches to it
+
+            // Give your customer a method called eatSandwich(). The method should take in a parameter of a sandwich and should write to the console a sentence like "mmm, what a delicious sandwich!"
+
+            // Move your sandwich and customer classes into seperate files 
+
+          // Talk about constructor methods 
+
+          // Talk about access modifiers 
+            
+            
+        }
+    }
+}
